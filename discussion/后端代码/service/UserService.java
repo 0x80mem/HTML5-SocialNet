9eeff57e01c9f245f3b3bc47c206c187159a777b
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-10 17:16:13 +0800

    Create UserService.java

---
+++ discussion/后端代码/service/UserService.java
@@ -0,0 +1,7 @@
+package com.jlusw.html.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jlusw.html.entity.User;
+
+public interface UserService extends IService<User> {
+}
