8c86fed00d1b99cc4ebca052653e998d23ce032c
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-10 19:48:32 +0800

    Update User.java
    User@Data注解

--- discussion/后端代码/entity/User.java
+++ discussion/后端代码/entity/User.java
@@ -4,33 +4,11 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Getter;
 
-@Getter
+@Data
 @TableName("user")
 public class User {
     @TableId(value = "id",type=IdType.AUTO)
     private int id;
-
     private String name;
     private String password;
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
