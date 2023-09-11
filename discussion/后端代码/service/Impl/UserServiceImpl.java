0d9f2b6c3156a6efa08585df766a06aab0e58950
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-11 21:46:19 +0800

    Update UserServiceImpl.java

--- discussion/后端代码/service/Impl/UserServiceImpl.java
+++ discussion/后端代码/service/Impl/UserServiceImpl.java
@@ -4,8 +4,21 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jlusw.html.entity.User;
 import com.jlusw.html.mapper.UserMapper;
 import com.jlusw.html.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public boolean cancel(long userId) {
+        User user=getById(userId);
+        if(user==null)
+            return false;
+        userMapper.deleteById(userId);
+        return userMapper.cancel(userId, user.getName(), LocalDateTime.now());
+    }
 }
