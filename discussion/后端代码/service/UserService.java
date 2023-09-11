32943a94208ad706ea336191eb4b9592b2b78cc8
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-11 21:45:29 +0800

    Update UserService.java

--- discussion/后端代码/service/UserService.java
+++ discussion/后端代码/service/UserService.java
@@ -4,4 +4,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.jlusw.html.entity.User;
 
 public interface UserService extends IService<User> {
+    /**
+     * 用户注销
+     * @param userId 注销用户id
+     * @return 注销结果
+     */
+    boolean cancel(long userId);
 }
