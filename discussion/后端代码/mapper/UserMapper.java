e7a234efc61de6d853de591a7f11c9f51e54dd7b
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-11 21:37:22 +0800

    Update UserMapper.java

--- discussion/后端代码/mapper/UserMapper.java
+++ discussion/后端代码/mapper/UserMapper.java
@@ -2,13 +2,19 @@ package com.jlusw.html.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jlusw.html.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User>{
+    /**
+     * 用户注销
+     * @param id 用户id
+     * @param name 用户名
+     * @param time 注销时间
+     * @return 注销结果
+     */
+    @Insert("insert into cancelled_user values(#{id},#{name},#{time})")
+    boolean cancel(long id, String name, LocalDateTime time);
 }
