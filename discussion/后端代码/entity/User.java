ea2d8cbe9cabf33a161b7d0a5b5a4cd47954a619
ShacooKL <130219816+ShacooKL@users.noreply.github.com>
2023-09-11 21:18:53 +0800

    Update User.java
    增加注册时间
    id  数据类型由int变为long

--- discussion/后端代码/entity/User.java
+++ discussion/后端代码/entity/User.java
@@ -3,14 +3,15 @@ package com.jlusw.html.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 
 @Data
 @TableName("user")
 public class User {
     @TableId(value = "id",type=IdType.AUTO)
-    private int id;
+    private long id;
     private String name;
     private String password;
-
+    private LocalDateTime registerTime;
 }
