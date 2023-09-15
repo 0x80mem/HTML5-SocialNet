841a2223a8188669475c288feeb1058f15779535
0x80mem <93927938+0x80mem@users.noreply.github.com>
2023-09-15 10:45:01 +0800

    Add files via upload

---
+++ 前端代码/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+ <nav>
+  <router-link to="/login" href="#">登录</router-link> |
+  <router-link to="/" href="#">推荐</router-link>
+</nav>
+<router-view />
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+
+<script>
+</script>
