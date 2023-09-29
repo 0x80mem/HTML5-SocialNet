0188f9d56c1e801b9919e577b60621bc194d4144
Du Boyuan <93927938+0x80mem@users.noreply.github.com>
2023-09-29 22:13:13 +0800

    三个组件

---
+++ 前端代码/src/components/RecNode.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-for="sub in post.parPost" :key="sub.id" :sub="sub">
+        <div v-if="key != parNode.id">
+            <RecNode :post="sub" :parNode="post" :getFunc="getFunc" :deleteFunc="deleteFunc"></RecNode>
+        </div>
+    </div>
+    <post-content :content="post.content" :showLevel="post.showLevel" :type="post.type"></post-content>
+    <div v-for="sub in post.chiPost" :key="sub.id" :sub="sub">
+        <div v-if="key != parNode.id">
+            <RecNode :post="sub" :parNode="post" :getFunc="getFunc" :deleteFunc="deleteFunc"></RecNode>
+        </div>
+    </div>
+</template>
+
+<script>
+import { createApp,  } from 'vue';
+import PostContent from './PostContent.vue';
+import store from '../store';
+import Post from '@/scripts/Post';
+
+const app = createApp();
+app.use(PostContent);
+  
+export default {
+  store,
+  name: 'RecNode',
+  components: {
+    PostContent,
+  },
+  props: {
+    post: {
+        type: Post,
+        default: (post) => post
+    },
+    parNode: {
+        type: Post,
+        default: (post) => post
+    },
+    getFunc: {
+        type: Function,
+    },
+    deleteFunc: {
+        type: Function,
+    }
+  },
+  setup() {
+    return {
+    }; 
+  },
+};
+</script>
