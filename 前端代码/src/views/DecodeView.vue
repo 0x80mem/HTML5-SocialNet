7b88b1cf294213de8dc642158556026a8772dfa4
114514Cheng <143814637+114514Cheng@users.noreply.github.com>
2023-10-10 21:00:18 +0800

    Post editor (#15)
    * Update DecodeView.vue
    
    前端小更新，更新了DecodeView还有test_case1，界面仍需完善
    
    * Update test_case1.js

--- 前端代码/src/views/DecodeView.vue
+++ 前端代码/src/views/DecodeView.vue
@@ -1,36 +1,65 @@
 <template>
-    <div v-if="type == 'recommend'">
+    <div>
+      <div v-if="type == 'recommend'">
         <recommend-view></recommend-view>
-    </div>
-    <div v-else-if="type == 'id'">
+      </div>
+      <div v-else-if="type == 'id'">
+        <van-popup
+          v-model:show="showBottom"
+          position="bottom"
+          :style="{ height: '50%' }"
+        >
+          <EditorView />
+        </van-popup>
         <post-tree :id="id" showLevel="full"></post-tree>
+        <button @click="showBottom = true" class="edit-button">写文章</button>
+      </div>
+      <div v-else>
+        <button @click="showBottom = false" class="edit-button">写文章</button>
+      </div>
     </div>
-</template>
-  
-<script>
+  </template>
 
-import { useRoute } from "vue-router"
-import RecommendView from './RecommendView.vue';
-import PostTree from "@/components/PostTree.vue";
+  <script>
+  import { useRoute } from "vue-router";
+  import RecommendView from './RecommendView.vue';
+  import PostTree from "@/components/PostTree.vue";
+  import EditorView from './EditorView.vue';
+  import {ref} from "vue";
+  export default {
+    components: {
+      RecommendView,
+      PostTree,
+      EditorView,
+    },
+    setup() {
+      const route = useRoute();
+      const showBottom = ref(false);
+      let type = "recommend";
+      let id = 1;
 
-export default{
-  components: { 
-    RecommendView,
-    PostTree,
-  },
-  setup()
-  {
-      const route = useRoute()
-      let type = "recommend"
-      let id = 1
-      if (route.query.id != undefined)
-          type = "id"
-          id = route.query.id
-      return {
-          type,
-            id,
+      if (route.query.id !== undefined) {
+        type = "id";
+        id = route.query.id;
       }
-  }
-}
 
-</script>
+
+      let isLogin = false;
+
+      return {
+        type,
+        id,
+        isLogin,
+        showBottom,
+      };
+    },
+  };
+  </script>
+
+  <style>
+  .edit-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  </style>
