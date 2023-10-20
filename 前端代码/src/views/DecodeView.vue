0e7437ee344a24daad8dd1d1c2e8e293390c446d
Du Boyuan <93927938+0x80mem@users.noreply.github.com>
2023-10-20 19:46:49 +0800

    Add files via upload

--- 前端代码/src/views/DecodeView.vue
+++ 前端代码/src/views/DecodeView.vue
@@ -1,36 +1,54 @@
 <template>
-    <div v-if="type == 'recommend'">
+    <div>
+      <div v-if="type == 'recommend'">
         <recommend-view></recommend-view>
+      </div>
+      <div v-else-if="type == 'id'">
+        <user-info-view :id="id" />
+      </div>
+      <div v-else>
+      </div>
     </div>
-    <div v-else-if="type == 'id'">
-        <post-tree :id="id" showLevel="full"></post-tree>
-    </div>
-</template>
-  
-<script>
+  </template>
 
-import { useRoute } from "vue-router"
-import RecommendView from './RecommendView.vue';
-import PostTree from "@/components/PostTree.vue";
+  <script>
+  import { useRoute } from "vue-router";
+  import RecommendView from './RecommendView.vue';
+  import {ref} from "vue";
+  import UserInfoView from './UserInfoView.vue';
+  export default {
+    components: {
+      RecommendView,
+      UserInfoView,
+    },
+    setup() {
+      const route = useRoute();
+      const showBottom = ref(false);
+      let type = "recommend";
+      let id = 1;
 
-export default{
-  components: { 
-    RecommendView,
-    PostTree,
-  },
-  setup()
-  {
-      const route = useRoute()
-      let type = "recommend"
-      let id = 1
-      if (route.query.id != undefined)
-          type = "id"
-          id = route.query.id
-      return {
-          type,
-            id,
+      if (route.query.id !== undefined) {
+        type = "id";
+        id = route.query.id;
       }
-  }
-}
 
-</script>
+
+      let isLogin = false;
+
+      return {
+        type,
+        id,
+        isLogin,
+        showBottom,
+      };
+    },
+  };
+  </script>
+
+  <style>
+  .edit-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  </style>
