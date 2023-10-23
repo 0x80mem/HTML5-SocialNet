f6bd03e15c7fddf4b4cf63c6257bba9d037f1dc1
114514Cheng <143814637+114514Cheng@users.noreply.github.com>
2023-10-23 19:36:11 +0800

    Create RichEditor.vue
    修改的文件：test_case1.js、RecommendView.vue、ArticlePost.vue
    添加的文件：RichEditor.vue、editorConfig.json

---
+++ 前端代码/src/components/RichEditor.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <div class="editor-title-container">
+        <input v-model="title" class="editor-title" placeholder="在这里输入标题" />
+      </div>
+      <Editor v-model="content" name="content" id="content" title="Editor" :api-key="apiKey" :init="editorConfig"></Editor>
+    </div>
+  </template>
+
+  <script>
+  import Editor from "@tinymce/tinymce-vue";
+
+  import { ref, reactive, toRefs } from 'vue';
+
+  export default {
+    components: {
+      Editor,
+    },
+    props: {
+      editorConfig: Object, // 通过props接收编辑器配置
+    },
+    setup() {
+      const content = ref("");
+      const title = ref("");
+      const apiKey = "zsbmp4e9znnl7a3rjlcj83ak105fquww17uttqptbt66e39e";
+
+      const tiny = reactive({
+        apiKey: apiKey,
+      });
+
+      return {
+        content,
+        ...toRefs(tiny),
+        title,
+        apiKey,
+      };
+    },
+  };
+  </script>
+
+  <style scoped>
+  .editor-title-container {
+    width: 100%;
+    border: 1px solid transparent;
+    border-radius: 5px;
+  }
+
+  .editor-title {
+    width: 100%;
+    border: none;
+    outline: none;
+    padding: 10px;
+    background: #f5f5f5; /* 更浅的灰色背景 */
+  }
+  </style>
