c3fbf6e31ca33f390ef8561145e352eda52ee3fa
Du Boyuan <93927938+0x80mem@users.noreply.github.com>
2023-10-24 13:57:57 +0800

    关注粉丝功能独立化 (#23)
    * Create FansPost
    
    粉丝组件
    
    * Update RecNode.vue
    
    向简单工厂增加粉丝，关注功能组件
    
    * Create SubscribePost
    
    关注功能组件
    
    * Update UserInfoPost.vue
    
    修改样式
    
    ---------
    
    Co-authored-by: lifushuo <143823868+lifushuo@users.noreply.github.com>

--- 前端代码/src/components/RecNode.vue
+++ 前端代码/src/components/RecNode.vue
@@ -1,15 +1,21 @@
 <template>
-    <div v-if="typeof(post) == 'object'">
-        <div v-if="post.type == 'post'">
-          <article-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></article-post>
-        </div>
-        <div v-else-if="post.type == 'user'">
-          <user-info-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></user-info-post>
-        </div>
-        <div v-else>
-          <default-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></default-post>
-        </div>
-    </div>
+  <div v-if="typeof(post) == 'object'">
+      <div v-if="post.type == 'post'">
+        <article-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></article-post>
+      </div>
+      <div v-else-if="post.type == 'user'">
+        <user-info-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></user-info-post>
+      </div>
+      <div v-else-if="post.type == 'subscribe'">
+        <subscribe-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></subscribe-post>
+      </div>
+      <div v-else-if="post.type == 'fans'">
+        <Fans-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></Fans-post>
+      </div>
+      <div v-else>
+        <default-post :post="post" :parNode="parNode" :visit="visit" :getFunc="getFunc" :deleteFunc="deleteFunc"></default-post>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -18,38 +24,42 @@ import store from '../store';
 import DefaultPost from './DefaultPost.vue';
 import ArticlePost from './ArticlePost.vue';
 import UserInfoPost from './UserInfoPost.vue';
-
+import SubscribePost  from './SubscribePost.vue';
+import FansPost  from './FansPost.vue';
 const app = createApp();
 app.use(DefaultPost);
-  
+app.use(SubscribePost);
+app.use(FansPost);
 export default {
-  store,
-  name: 'RecNode',
-  components: {
-    DefaultPost,
-    ArticlePost,
-    UserInfoPost,
+store,
+name: 'RecNode',
+components: {
+  DefaultPost,
+  ArticlePost,
+  UserInfoPost,
+  SubscribePost,
+  FansPost,
+},
+props: {
+  post: {
+      type: Object,
   },
-  props: {
-    post: {
-        type: Object,
-    },
-    parNode: {
-        type: Object,
-    },
-    visit: {
-        type: Set,
-    },
-    getFunc: {
-        type: Function,
-    },
-    deleteFunc: {
-        type: Function,
-    }
+  parNode: {
+      type: Object,
   },
-  setup() {
-    return {
-    }; 
+  visit: {
+      type: Set,
   },
+  getFunc: {
+      type: Function,
+  },
+  deleteFunc: {
+      type: Function,
+  }
+},
+setup() {
+  return {
+  }; 
+},
 };
 </script>
