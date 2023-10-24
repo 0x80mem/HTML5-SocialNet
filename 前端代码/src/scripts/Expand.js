9639a3afac7b70ce0ad70c49dc5dccb43e681f85
Du Boyuan <93927938+0x80mem@users.noreply.github.com>
2023-10-24 18:31:43 +0800

    修改了Expand深拷贝子节点时的bug

--- 前端代码/src/scripts/Expand.js
+++ 前端代码/src/scripts/Expand.js
@@ -1,4 +1,5 @@
 import ShowLevel from "./ShowLevel"
+import TypeConfig from "./TypeConfig"
 
 // 目前改变子节点显示不影响父节点
 function Expand(post, visit, showLevel, getFunc, deleteFunc)
@@ -20,11 +21,12 @@ function Expand(post, visit, showLevel, getFunc, deleteFunc)
         return post
 
     // 递归子节点
-    let chiShow = post.type.chi_policy[showLevel]
+    let type = TypeConfig[post.type]
+    let chiShow = type.chi_policy[showLevel]
     // 无子节点
     if (typeof post.chiPost[0] == 'number')
     {
-        let chilist = post.chiPost.copy()
+        let chilist = [...post.chiPost]
         post.chiPost = []
         if (chiShow > ShowLevel['hide'])
         {
@@ -43,7 +45,7 @@ function Expand(post, visit, showLevel, getFunc, deleteFunc)
         let chiNow = post.chiPost[0].showLevel
         if (chiNow !== chiShow)
         {
-            let chilist = post.chiPost.copy()
+            let chilist = [...post.chiPost]
             post.chiPost = []
             chilist.forEach(chi => {
                 post.chiPost.push(Expand(chi, visit, chiShow, getFunc, deleteFunc))
