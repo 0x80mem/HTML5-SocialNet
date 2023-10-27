ea8466f623359b584de94fcdd24326fdaf866999
Du Boyuan <93927938+0x80mem@users.noreply.github.com>
2023-10-27 20:29:13 +0800

    Update HomeTool.vue

--- 前端代码/src/components/HomeTool.vue
+++ 前端代码/src/components/HomeTool.vue
@@ -1,22 +1,25 @@
 <template>
-    <div>
-      <van-button @click="goHome" icon="home-o"
-      style="width: 30px; height: 30px;"
-      ></van-button>
-    </div>
+  <div>
+    <van-button
+      @click="goHome"
+      icon="home-o"
+      tips="主页"
+      style="width: 30px; height: 30px"
+    ></van-button>
+  </div>
 </template>
   
 <script>
-import { Button } from 'vant';
-  
+import { Button } from "vant";
+
 export default {
   components: {
-    [Button.name]: Button
+    [Button.name]: Button,
   },
-    methods: {
-      goHome() {
-        window.location.href = '/';
-      }
-    }
+  methods: {
+    goHome() {
+      window.location.href = "/";
+    },
+  },
 };
 </script>
